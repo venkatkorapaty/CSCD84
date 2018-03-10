7cef2d8cc2172cdf50559ba663e1308987d65fff
Shevlin Darshan Bisesar <bisesars@IITS-I406-30.utsc-labs.utoronto.ca>
2018-03-10 16:29:56 -0500

    we did some ting

--- D84-A3/Distro/QLearn.c
+++ D84-A3/Distro/QLearn.c
@@ -108,7 +108,7 @@ int QLearn_action(double gr[max_graph_size][4], int mouse_pos[1][2], int cats[5]
     // int out_y = new_mouse[1] >= 0 && new_mouse[1] < size_X;
     //  && (out_y) && (out_x)
         3 - move left
-        
+        int distCat(double gr[max_graph_size][4], int mouse_pos[1][2], int cats[5][2], int cheeses[5][2], int size_X, int graph_size)
      QLearn_core_GL will print a warning if your action makes the mouse cross a wall, or if it makes
      the mouse leave the map - this should not happen. If you see a warning, fix the code in this
      function!
@@ -261,7 +261,7 @@ double QLearn_reward(double gr[max_graph_size][4], int mouse_pos[1][2], int cats
   return(0);
   // return(fabs(mouse_pos[0][0] - cats[0][0]) + fabs(mouse_pos[0][1] - cats[0][1]) - (fabs(mouse_pos[0][0] - cheeses[0][0]) + fabs(mouse_pos[0][1] - cheeses[0][1])));		// <--- of course, you will change this as well!     
 }
-
+static int g_graph_size = 0;
 void feat_QLearn_update(double gr[max_graph_size][4],double weights[25], double reward, int mouse_pos[1][2], int cats[5][2], int cheeses[5][2], int size_X, int graph_size)
 {
   /*
@@ -280,6 +280,7 @@ void feat_QLearn_update(double gr[max_graph_size][4],double weights[25], double 
 
   //create and evaluate the features array
   // fprintf(stderr, "PACOOO..");
+  g_graph_size = graph_size;
   double * features = (double*)malloc(sizeof(double)*25);
   evaluateFeatures(gr, features, mouse_pos, cats, cheeses, size_X, graph_size);
   int *a;
@@ -292,15 +293,15 @@ void feat_QLearn_update(double gr[max_graph_size][4],double weights[25], double 
 
   maxQsa(gr, weights, mouse_pos, cats, cheeses, size_X, graph_size, max, a);
   int new_mouse[2] = {mouse_pos[0][0], mouse_pos[0][1]};
-  if (*a % 2 == 0) {
-    new_mouse[1] -= 1 - *a;
+  if (act % 2 == 0) {
+    new_mouse[1] -= 1 - act;
   } else {
-    new_mouse[0] += 2 - *a;
+    new_mouse[0] += 2 - act;
   }
   double qs = Qsa(weights, features);
   // fprintf(stderr, "..WHYYY2\n");
   for (int i = 0; i < 25; i++) {
-    weights[i] += alpha*(reward + (lambda * (*max)) - qs) * features[i];
+    weights[i] += alpha*(reward + (lambda * (max_val)) - qs) * features[i];
   } 
 
   free(features);
@@ -323,7 +324,7 @@ int feat_QLearn_action(double gr[max_graph_size][4],double weights[25], int mous
     the maze.    
    */
 
-  /***********************************************************************************************
+  /********************************************************************int***************************
    * TO DO: Complete this function
    ***********************************************************************************************/        
   //observer current state
@@ -339,21 +340,21 @@ int feat_QLearn_action(double gr[max_graph_size][4],double weights[25], int mous
   double max_val = 0.0;
   val = &max_val; 
   // fprintf(stderr, "%f\n", pct);
-  if (prob_rand > pct) {
+  if (prob_rand >= pct) {
     do {
       // do rand act that's also valid
-      *a = rand()%4;
-    } while(!(gr[mouse_pos[0][0] + size_X*mouse_pos[0][1]][*a]));
+      act = rand()%4;
+    } while(!(gr[mouse_pos[0][0] + size_X*mouse_pos[0][1]][act]));
   }
   else {
     maxQsa(gr, weights, mouse_pos, cats, cheeses, size_X, graph_size, val, a);
   }
   //find new mouse coords given the action we decided to take
   int new_mouse[2] = {mouse_pos[0][0], mouse_pos[0][1]};
-  if (*a % 2 == 0) {
-    new_mouse[1] -= 1 - *a;
+  if (act % 2 == 0) {
+    new_mouse[1] -= 1 - act;
   } else {
-    new_mouse[0] += 2 - *a;
+    new_mouse[0] += 2 - act;
   }
   //receive immediate reward
   int fuckyou[1][2] = {{new_mouse[0], new_mouse[1]}};
@@ -382,17 +383,18 @@ void evaluateFeatures(double gr[max_graph_size][4],double features[25], int mous
    Note that instead of passing down the number of cats and the number of cheese chunks (too many parms!)
    the arrays themselves will tell you what are valid cat/cheese locations.
    
+    if (i % 2 == 0) {
    You can have up to    // int out_x = new_mouse[0] >= 0 && new_mouse[0] < size_X;
     // int out_y = new_mouse[1] >= 0 && new_mouse[1] < size_X;
     //  && (out_y) && (out_x) 5 cats and up to 5 cheese chunks, and array entries for the remaining cats/cheese
    will have a value of -1 - check this when evaluating your features!
   */
 
-   /***********************************************************************************************
-   * TO DO: Complete this function
-   ***********************************************************************************************/      
-  features[0] = distCheese(gr, mouse_pos, cats, cheeses, size_X, graph_size);
-  for (int i = 1; i < 25; i++) {
+  features[2] = distCheese(gr, mouse_pos, cats, cheeses, size_X, graph_size);
+  features[1] = distCat(gr, mouse_pos, cats, cheeses, size_X, graph_size);
+  features[0] = mouseWalls(gr, mouse_pos, cats, size_X);
+  // features[0] = findAmountPaths(gr, mouse_pos, cats, cheeses, graph_size);
+  for (int i = 3; i < 25; i++) {
     features[i] = 0;
   }
    
@@ -404,9 +406,6 @@ double Qsa(double weights[25], double features[25])
     Compute and return the Qsa value given the input features and current weights
    */
 
-  /***********************************************************************************************
-  * TO DO: Complete this function
-  ***********************************************************************************************/  
   double ret = 0;
   //GRADIENT DESCENT
   for (int i = 0; i < 25; i++) {
@@ -427,7 +426,7 @@ void maxQsa(double gr[max_graph_size][4],double weights[25],int mouse_pos[1][2],
   */
  
    /***********************************************************************************************
-   * TO DO: Complete this function
+   * TO DO: Complete this functionint size_X
    ***********************************************************************************************/  
  
   *maxU = -100000.0;
@@ -470,7 +469,354 @@ void maxQsa(double gr[max_graph_size][4],double weights[25],int mouse_pos[1][2],
  *  Add any functions needed to compute your features below 
  *                 ---->  THIS BOX <-----
  * *************************************************************************************************/
-int distCheese(double gr[max_graph_size][4], int mouse_pos[1][2], int cats[5][2], int cheeses[5][2], int size_X, int graph_size) {
+double distCheese(double gr[max_graph_size][4], int mouse_pos[1][2], int cats[5][2], int cheeses[5][2], int size_X, int graph_size) {
   //test code
-  return abs(mouse_pos[0][0] - cheeses[0][0]) + abs(mouse_pos[0][1] - cheeses[0][1]);
+  double shortest = 1000.0;
+  for (int i = 0; i < 5; i++) {
+    if (cheeses[i][0] < 0 || cheeses[i][1] < 0) {
+      continue;
+    }
+    double temp = pow(pow(fabs(mouse_pos[0][0] - cheeses[i][0]), 2) + pow(fabs(mouse_pos[0][1] - cheeses[i][1]), 2),0.5);
+    if (temp < shortest) {
+      shortest = temp;
+    }
+  }
+  return 10/(1 + shortest);
+}
+
+
+double distCat(double gr[max_graph_size][4], int mouse_pos[1][2], int cats[5][2], int cheeses[5][2], int size_X, int graph_size) {
+  //test code
+  double shortest = 1000.0;
+  for (int i = 0; i < 5; i++) {
+    if (cats[i][0] < 0 || cats[i][1] < 0) {
+      continue;
+    }
+    double temp = pow(pow(fabs(mouse_pos[0][0] - cats[i][0]), 2) + pow(fabs(mouse_pos[0][1] - cats[i][1]), 2),0.5);
+    if (temp < shortest) {
+      shortest = temp;
+    }
+  }
+  return 1/(1+ shortest);
+}
+
+double mouseWalls(double gr[max_graph_size][4], int mouse_pos[1][2], int cats[5][2], int size_X) {
+  int walls = 0;
+  
+  for (int i = 0; i < 4; i++) {
+    int new_mouse[2] = {mouse_pos[0][0], mouse_pos[0][1]};
+    if (i % 2 == 0) {
+      new_mouse[1] -= 1 - i;
+    } else {
+      new_mouse[0] += 2 - i;
+    }
+    if (!gr[mouse_pos[0][0] + size_X*mouse_pos[0][1]][i]) {
+      walls++;
+    } else {
+      for (int i = 0; i < 5; i++) {
+        if (new_mouse[0] == cats[i][0] && new_mouse[1] == cats[i][1]) {
+          walls++;
+        }
+      }
+    }
+  }
+  return (double)walls;
+}
+
+double findAmountPaths(double gr[max_graph_size][4], int mouse_pos[1][2], int cats[5][2], int cheeses[5][2], int graph_size) {
+  double amountOfPaths = 0.0;
+
+  int size_X = (int)sqrt(graph_size);
+  int *pred = (int*)malloc(sizeof(int)*graph_size);
+  int path[max_graph_size][2];
+  for (int i = 0; i < graph_size; i++) {
+    path[i][0] = -1;
+    path[i][1] = -1;
+  }
+  int amountOfCats = 0;
+  int amountOfCheeses = 0;
+  for (int i = 0; i < 5; i++) {
+    if (cats[i][0] != -1)
+      amountOfCats++;
+    if (cheeses[i][0] != -1)
+      amountOfCheeses++;
+  }
+  // find optimial path from mouse location to every other spot
+  findDeWay(gr, path, cats, amountOfCats, cheeses, amountOfCheeses, mouse_pos, pred, graph_size);
+
+  // fprintf(stderr, "start\n");
+  // for (int i = 0; i < graph_size; i++) {
+  //   fprintf(stderr, "%d: %d\n", i, pred[i]);
+  // }
+
+  int wallsAroundCheese[amountOfCheeses];
+  int priority_cheese = 0;
+
+  // find amount of walls around each cheese
+  for (int cheese = 0; cheese < amountOfCheeses; cheese++) {
+    for (int wall = 0; wall < 4; wall++) {
+      if (!gr[cheeses[cheese][0] + cheeses[cheese][1]*size_X][wall])
+        wallsAroundCheese[cheese]++;
+    }
+  }
+
+  // find cheese with least amount of walls
+  int resultWalls = 4;
+  for (int cheese = 0; cheese < amountOfCheeses; cheese++) {
+    if (wallsAroundCheese[cheese] < resultWalls) {
+      resultWalls = wallsAroundCheese[cheese];
+      priority_cheese = cheese;
+    }
+  }
+
+  int pathLen = traceBack2(pred, getLocation(cheeses[priority_cheese], size_X), mouse_pos[0], graph_size, path, mouse_pos);
+
+  int mansNotHOt = abs(mouse_pos[0][0] - cheeses[priority_cheese][0]) + abs(mouse_pos[0][1] - cheeses[priority_cheese][1]);
+
+  if (pathLen >= mansNotHOt) {
+    amountOfPaths += 1.0;
+  }
+
+  return amountOfPaths;
+}
+
+int traceBack2(int pred[max_graph_size], int current, int origin[2], int graph_size, int path[max_graph_size][2], int mouse_pos[1][2]) {
+  int size_X = ((int)sqrt(graph_size));
+  int size_Y = size_X;
+	// uses the pred array to backtrack and figure out the path we found to the goal
+	int i = 0;
+	int reversePath[max_graph_size];
+
+  int goal = getLocation(origin, size_X);
+  // fprintf(stderr, "start: %d, goal %d\n", pred[current], goal);
+	while (pred[current] != -1 && current != goal) {
+    // fprintf(stderr, "%d\n", i);
+		reversePath[i] = current;
+		current = pred[current];
+		i++;
+	}
+
+	// int x;
+	// int y;
+	// for (int j = 0; j < i; j++) {
+	// 	x = reversePath[j]%size_X;
+	// 	y = reversePath[j]/size_Y;
+	// 	path[i - j][0] = x;
+	// 	path[i - j][1] = y;
+  //   fprintf(stderr, "path loc: (%d, %d)\n",x, y);
+	// }
+	// path[0][0] = mouse_pos[0][0];
+	// path[0][1] = mouse_pos[0][1];
+
+	return i;
+}
+
+void addHeap(int *heap, double weights[max_graph_size], int actWeights[max_graph_size], int val, int size, int graph_size) {
+	// adds val to our heap data structure for the A* algorithms
+	int parent;
+	while (size > 0) {
+		parent = (size - 1) / 2;
+		
+		if (weights[val] + actWeights[val] >= weights[heap[parent]] + actWeights[heap[parent]]) {
+			heap[size] = val;
+			return;
+		}
+		heap[size] = heap[parent];
+		size = parent;
+	}
+	heap[0] = val;
+}
+
+void propagate(int *heap, double weights[max_graph_size], int actWeights[max_graph_size], int node, int size, int graph_size) {
+	// when extracting min from the heap we need to reorganize our heap
+	if (size == 2) {
+		if (weights[heap[0]] + actWeights[heap[0]] > weights[heap[1]] + actWeights[heap[1]]) {
+			int temp = heap[0];
+			heap[0] = heap[1];
+			heap[1] = temp;
+		}
+		return;
+	}
+	int l = node*2 + 1;
+	int r = node*2 + 2;
+	int min = weights[heap[node]] + actWeights[heap[node]];
+	int mindex = node;
+
+	if (l < size - 1 && (weights[heap[l]] + actWeights[heap[l]]) < (weights[heap[r]] + actWeights[heap[r]])) {
+		min = weights[heap[l]] + actWeights[heap[l]];
+		mindex = l;
+	}
+	if (r < size - 1 && (weights[heap[r]] + actWeights[heap[r]]) < (weights[heap[mindex]] + actWeights[heap[mindex]])) {
+		min = weights[heap[r]] + actWeights[heap[r]];
+		mindex = r;
+	}
+	if (mindex != node) {
+		int temp = heap[node];
+		heap[node] = heap[mindex];
+		heap[mindex] = temp;
+		propagate(heap, weights, actWeights, mindex, size, graph_size);
+	}
+}
+
+int extract_min(int *heap, double weights[max_graph_size], int actWeights[max_graph_size], int size, int graph_size) {
+	//get the first element in the heap and then re-sort it
+	int min;
+	int last_elem;
+	if (size == 0) {
+		return -1;
+	}
+	if (size == 1) {
+		min = heap[0];
+		heap[0] = -1;
+		return min;
+	}
+
+	min = heap[0];
+	last_elem = heap[size - 1];
+	heap[0] = last_elem;
+	heap[size - 1] = -1;
+
+	propagate(heap, weights, actWeights, 0, size - 1, graph_size);
+
+	return min;
+}
+
+int H_cost(int x, int y, int cat_loc[10][2], int cheese_loc[10][2], int mouse_loc[1][2], int cats, int cheeses, double gr[max_graph_size][4], int graph_size)
+{
+
+	int heur = 99999;
+	for (int i = 0; i < cheeses; i++) {
+		int eu_dist = abs(x - cheese_loc[i][0]) + abs(y - cheese_loc[i][1]);
+		if (heur > eu_dist) {
+			heur = eu_dist;
+		}
+	}
+	return heur;
+}
+
+void findDeWay(double gr[max_graph_size][4], int path[max_graph_size][2], int cat_loc[5][2], int cats, int cheese_loc[5][2], int cheeses, int mouse_loc[1][2], int *pred, int graph_size)
+{
+  int size_X = ((int)sqrt(graph_size));
+  int size_Y = size_X;
+	// // Stub so that the code compiles/runs - The code below will be removed and replaced by your code!
+	int visited[graph_size];
+	// int pred[graph_size];
+	//initialize arrays because of C
+	for (int i = 0; i < graph_size; i++) {
+		visited[i] = 0;
+		pred[i] = -1;
+	}
+
+	//declarations
+	int v = 1;
+	int queueMain[graph_size];
+	int queueIndex = 0;
+	int stackIndex = 0;
+	int *heap = (int*)malloc(sizeof(int)*(graph_size)*2);
+	double weights[graph_size];
+	int actWeights[graph_size];
+	int heap_visited[graph_size];
+
+
+	//set all the initial actual weights and visited nodes to 0 for A* modes
+	for (int i = 0; i < graph_size; i++) {
+		actWeights[i] = 0;
+		heap_visited[i] = 0;
+	}
+
+	actWeights[mouse_loc[0][0] + (mouse_loc[0][1]*size_X)] = -1;
+
+	//if A* we set all the elements in our heap to -1 and then add the first element (current location)
+	for (int i = 0; i < graph_size; i++) {
+		heap[i] = -1;
+	}
+	addHeap(heap, weights, actWeights, mouse_loc[0][0] + mouse_loc[0][1]*size_X, queueIndex, graph_size);
+	visited[mouse_loc[0][0] + (size_X*mouse_loc[0][1])] = 1;
+	queueIndex++;
+
+	int current = -1;
+
+	while (queueIndex > 0/*cheeses > 0*/) {
+		current = extract_min(heap, weights, actWeights, queueIndex, graph_size);
+		queueIndex--;
+		while (heap_visited[current] == 1) {
+			current = extract_min(heap, weights, actWeights, queueIndex, graph_size);
+			queueIndex--;
+		}
+
+		heap_visited[current] = 1;
+
+		int xCord = current % size_X;
+		int yCord = current / size_Y;
+
+		int catCheeseLoc = is_cat_or_cheese(xCord, yCord , cat_loc, cats, cheese_loc, cheeses);
+
+		v++;
+		double *loc = gr[xCord + (yCord*size_X)];
+		
+		// holds grid coordinates around our current coordinate
+		int adj_Cords[4][2] = {{xCord, yCord-1},
+		{xCord + 1, yCord},
+		{xCord, yCord + 1},
+		{xCord - 1, yCord}
+		};
+
+		for (int i = 0; i < 4; i++) {
+			int x = i;
+			// if valid location
+			if (loc[x]) {
+				// if we haven't visited
+				if(!visited[(adj_Cords[x][0]) + ((adj_Cords[x][1]) * size_Y)]){
+					int catCheeseLoc = is_cat_or_cheese(adj_Cords[x][0], adj_Cords[x][1], cat_loc, cats, cheese_loc, cheeses);
+          if (catCheeseLoc != 2) {// if not a cat
+            //add to heap and set weights
+            visited[(adj_Cords[x][0]) + (adj_Cords[x][1] * size_X)] = 1;
+            pred[(adj_Cords[x][0]) + (adj_Cords[x][1] * size_X)] = current;
+            if (actWeights[xCord + (yCord*size_Y)] == -1) {
+              //root is current (act weight is -1)
+              actWeights[adj_Cords[x][0] + ((adj_Cords[x][1]) * size_Y)] = 1;
+            } else {
+              actWeights[adj_Cords[x][0] + ((adj_Cords[x][1]) * size_Y)] = 1 + actWeights[xCord + (yCord*size_Y)];
+            }
+            weights[(adj_Cords[x][0]) + (adj_Cords[x][1] * size_X)] = H_cost(adj_Cords[x][0], adj_Cords[x][1], cat_loc, cheese_loc, mouse_loc, cats, cheeses, gr, graph_size);
+            addHeap(heap, weights, actWeights, (adj_Cords[x][0]) + (adj_Cords[x][1] * size_X), queueIndex, graph_size);
+            queueIndex++;
+          }
+				}
+				// When running UCS, and we come back to a node, see if it has faster path
+				else if (actWeights[xCord + ((yCord)*size_Y)] != -1) {
+					if (actWeights[adj_Cords[i][0] + ((adj_Cords[i][1]) * size_Y)] >= 1 + actWeights[xCord + (yCord*size_Y)]) {
+						actWeights[adj_Cords[i][0] + ((adj_Cords[i][1]) * size_Y)] = 1 + actWeights[xCord + (yCord*size_Y)];
+						pred[(adj_Cords[i][0]) + adj_Cords[i][1] * size_X] = current;
+						addHeap(heap, weights, actWeights, (adj_Cords[i][0]) + (adj_Cords[i][1] * size_X), queueIndex, graph_size);
+						queueIndex++;
+					}
+				}
+			}
+		}
+	}
+}
+
+int getLocation(int coords[2], int size_Y) {
+	return coords[0] + (coords[1]*size_Y);
+}
+
+int is_cat_or_cheese(int x, int y, int cat_loc[10][2], int cats, int cheese_loc[10][2], int cheeses) {
+	/*
+	  Return 0 for safe spot
+	  1 if cheese is there
+	  2 if fur devil is there
+	*/
+	int bound = fmax(cats, cheeses);
+	int i = 0;
+	while (i < bound) {
+		if (i < cats && cat_loc[i][0] == x && cat_loc[i][1] == y) {
+			return 2;
+		}
+		if (i < cheeses && cheese_loc[i][0] == x && cheese_loc[i][1] == y) {
+			return 1;
+		}
+		i++;
+	}
+	return 0;
 }
